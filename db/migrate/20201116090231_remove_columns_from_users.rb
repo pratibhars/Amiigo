468c82c7d73108adcf1172b81087e0d82266e744
Pratibha <68182689+pratibhars@users.noreply.github.com>
2020-11-17 18:33:06 +1000

    db reset and db migrate

--- db/migrate/20201116090231_remove_columns_from_users.rb
+++
@@ -1,8 +0,0 @@
-class RemoveColumnsFromUsers < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :users, :uid, :string
-    remove_column :users, :provider, :string
-    remove_column :users, :access_code, :string
-    remove_column :users, :publishable_key, :string
-  end
-end
