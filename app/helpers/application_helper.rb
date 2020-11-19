7bd124a5876cf41464e5edab387ddf5297c782e0
Pratibha <68182689+pratibhars@users.noreply.github.com>
2020-11-19 17:15:56 +1000

    git commit -m "changed views added card"

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,20 @@
 module ApplicationHelper
     def author_of(resource)
         user_signed_in? && resource.user_id == current_user.id
+    end
+
+    #confirm user signed in is admin
+    def admin_confirmed
+        user_signed_in? && current_user.admin_role?
+    end  
+
+    #confirm user signed in is seller
+    def seller_confirmed
+        user_signed_in? && current_user.seller_role?
     end 
 
-    def admin?
-        user_signed_in? && current_user.admin?
-    end  
+    #confirm used signed in is customer 
+    def customer_confirmed
+        user_signed_in? && current_user.customer_role?
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,5 @@ Rails.application.routes.draw do
 
   end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "amiigos#index"
+  root to: "amiibos#index"
 end
